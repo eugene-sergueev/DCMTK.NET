1ddd9681630db6bb47d6667d49c3a868aba70da2
Paul Knopf <theonlylawislove@gmail.com>
2015-02-15 11:27:41 -0500

    Fixed release build

--- src/DCMTK.NET/DcmDatasetNet.h
+++ src/DCMTK.NET/DcmDatasetNet.h
@@ -15,41 +15,11 @@ public:
 	OFConditionNet^ FindAndGetString(DcmTagKeyNet^ tagKey, unsigned long position, bool searchIntoSub, [System::Runtime::InteropServices::Out] System::String^% result)
 	{
 		result = nullptr;
-
 		OFString string;
+
 		OFCondition cond = _dataSet->findAndGetOFString(tagKey->GetUnmanagedType(), string, 0, searchIntoSub);
 		if (cond.good())
-		{
 			result = gcnew System::String(string.c_str());
-		}
-
-		/*DcmSequenceOfItems* sequence;
-		cond = _dataSet->findAndGetSequence(DCM_ScheduledProcedureStepSequence, sequence, true, false);
-		if (cond.good())
-		{
-			DcmStack stack;
-			while (sequence->nextObject(stack, true).good())
-			{
-				DcmObject* item = stack.top();
-				if (item->getTag() == DCM_ScheduledProcedureStepStartDate)
-				{
-					DcmElement* element = (DcmElement*)item;
-					element->getOFString(string, 0, true);
-					result = gcnew System::String(string.c_str());
-				}
-			}
-		}*/
-
-		//DCMNET_WARN(DcmObject::PrintHelper(*_dataSet));
-		/*if (!elementList->empty())
-		{
-			DcmObject *dO;
-			elementList->seek(ELP_first);
-			do {
-				dO = elementList->get();
-				dO->print(out, flags, level + 1, pixelFileName, pixelCounter);
-			} while (elementList->seek(ELP_next));
-		}*/
 
 		return gcnew OFConditionNet(cond);
 	}
